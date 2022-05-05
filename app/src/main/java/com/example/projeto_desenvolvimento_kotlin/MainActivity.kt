3e2f56e2433e63f8c3695ab46786a11cce1a97ed
Vinicius Nunes <viniiciuspereiira@hotmail.com>
2022-05-04 23:17:32 -0300

    wip: login with facebook and firebase authentication

--- app/src/main/java/com/example/projeto_desenvolvimento_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/projeto_desenvolvimento_kotlin/MainActivity.kt
@@ -3,38 +3,44 @@ package com.example.projeto_desenvolvimento_kotlin
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
+import com.facebook.CallbackManager
+import java.util.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private val callbackManager = CallbackManager.Factory.create()
+    private val EMAIL = "email"
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // var binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        val loginButton: Button = findViewById(R.id.loginButton)
 
-        ShowMessageAsyncTask().execute()
+//        loginButton.setReadPermissions(Arrays.asList((EMAIL)))
+//        val loginButton: Button = findViewById(R.id.login_button)
+
+
     }
 
-    inner class ShowMessageAsyncTask() : AsyncTask<String, Int, Boolean>() {
-
-
-
-        override fun doInBackground(vararg params: String?): Boolean? {
-            startProcess()
-            return true
-        }
-
-        override fun onPostExecute(result: Boolean?) {
-            super.onPostExecute(result)
-            Toast.makeText(this@MainActivity, "Carregado com sucesso", Toast.LENGTH_LONG).show()
-        }
-
-        override fun onProgressUpdate(vararg values: Int?) {
-            super.onProgressUpdate(*values)
-        }
-    }
-
-    private fun startProcess() {
-        Thread.sleep(2000)
-    }
+//    inner class ShowMessageAsyncTask() : AsyncTask<String, Int, String>() {
+//
+//        override fun doInBackground(vararg params: String?): String? {
+//            Thread.sleep(5000)
+//
+//            return "Thread de background executada com sucesso"
+//        }
+//
+//        override fun onPostExecute(result: String?) {
+//            super.onPostExecute(result)
+//            Toast.makeText(this@MainActivity, result, Toast.LENGTH_LONG).show()
+//        }
+//
+//        override fun onProgressUpdate(vararg values: Int?) {
+//            super.onProgressUpdate(*values)
+//        }
+//    }
 }
